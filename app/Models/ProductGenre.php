2378a337a16c91a50b92505ff97163d550d74fa4
RyoNagashiro9280 <kd1322303@st.kobedenshi.ac.jp>
2025-04-30 02:03:11 +0000

    ProductGenreモデル変更

--- app/Models/ProductGenre.php
+++ app/Models/ProductGenre.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class ProductGenre extends Model
 {
     //
+    protected $fillable = [
+        'id', 'product_id', 'genre_id'
+    ];
+    public function genre()
+    {
+        return $this->belongsTo(Genres::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
