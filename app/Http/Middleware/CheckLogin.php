92422c2a2979409522b54841059dc07d9a91f1a7
RyoNagashiro9280 <kd1322303@st.kobedenshi.ac.jp>
2025-05-07 04:52:27 +0000

    URL直で行かんようにしたやつ

---
+++ app/Http/Middleware/CheckLogin.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckLogin
+{
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return redirect()->route('showLogin'); // ログインページへリダイレクト
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,57 +1,42 @@
 <?php
-
-use App\models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReplyController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\AnswerController;
+use App\Http\Middleware\CheckLogin;
+
 //ログイン画面へのルート設定
 Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::get('/index', function () {
-    return view('index');
+//ログイン判定関係
+// ミドルウェア適用（ログイン必須のルート）
+Route::middleware(CheckLogin::class)->group(function () {
+    Route::get('/index', function () {
+        return view('index');
+    });
+
+    // 質問機能
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/answer', [PostController::class, 'answer'])->name('posts.answer');
+    Route::post('/posts/{post}/answers', [AnswerController::class, 'store'])->name('answers.store');
+
+    // 作品機能
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+    Route::post('/products/{product}/replies', [ReplyController::class, 'store'])->name('replies.store');
+
+    // ホーム画面
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
+
+    // ログアウト
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
-
-//パスワード変更画面へのルート設定
-Route::get('/change-password', [AuthController::class, 'chLogin'])->name('password.change');
-Route::post('/change-password', [AuthController::class, 'updatepassword'])->name('password.update');
-
-
-// 質問一覧のルーティング
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-// 質問投稿へのルーティング
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-
-// 作品一覧画面
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-
-// 投稿画面を表示する
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-
-// 投稿内容を保存する
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-
-// 制作詳細画面のルーティング
-Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-
-// 作品へのリプライ投稿処理
-Route::post('/products/{product}/replies', [ReplyController::class, 'store'])->name('replies.store');
-
-
-
-Route::get('home',function(){
-    return view('home');
-})->name('home')->middleware('auth');
-
-
-Route::post('logout',[AuthController::class, 'logout'])->name('logout');
-
-Route::post('/posts/{post}/answers', [AnswerController::class, 'store'])->name('answers.store');
-
-// 投稿の詳細・回答ページのルート
-Route::get('/posts/{post}/answer', [PostController::class, 'answer'])->name('posts.answer');
