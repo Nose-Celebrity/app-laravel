b147e811ca5e8c53432b29df60a45da879ad358f
sato <kd1371234@st.kobedenshi.ac.jp>
2025-05-22 05:10:50 +0000

    プロフィール編集

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,17 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/header.css') }}">
     <title>プロフィール編集</title>
 </head>
 <body>
     <header class="header">
-        <ul class="menu" style="list-style: none; padding-left: 0;">
-            <li><a class="select" href="{{ route('posts.index')}}">質問</a></li>
-            <li><a class="select" href="{{ route('products.index')}}">作品投稿</a></li>
-            <li><a class="select now" href="{{ route('profile.index') }}">マイプロフィール</a></li>
+        <div class="header-left">
+            <span class="site-title">情報共有サイト</span>
+        </div>
 
-        </ul>
+        <div class="header-right">
+            {{-- ユーザーアイコン＆メニュー --}}
+            <div class="user-menu-wrapper">
+                <img src="{{ asset(Auth::user()->photo ?? 'image/default_profile.png') }}"
+                    class="user-icon" alt="ユーザーアイコン" onclick="toggleUserMenu()">
+                <ul class="user-menu" id="userMenu">
+                    <li><a href="{{ route('profile.index') }}">マイプロフィール</a></li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">@csrf
+                            <button type="submit">ログアウト</button>
+                        </form>
+                    </li>
+                    <li>
+                        <form method="POST" action="{{ route('user.delete') }}" onsubmit="return confirm('本当に削除しますか？');">
+                            @csrf @method('DELETE')
+                            <button type="submit">パスワード変更</button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </header>
+    <nav class="breadcrumb">
+        <a href="{{ route('home') }}">ホーム</a> &gt;
+        <a href="{{ route('profile.index') }}">プロフィール</a> &gt;
+        <span class="current">プロフィール編集</span>
+    </nav>
 
     <div class="container">
         <h1>プロフィール編集</h1>
