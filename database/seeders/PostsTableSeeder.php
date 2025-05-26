658dd2035eb23e5eb51d306fc23c68d5c64b6267
kd1322303@st.kobedenshi.ac.jp <kd1322303@st.kobedenshi.ac.jp>
2025-05-26 01:38:02 +0000

    シーダー追加途中

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -14,13 +14,18 @@ class PostsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        $user = User::first();
 
         Post::create([
-            'user_id' => $user->id,
-            'title' => '最初の投稿',
-            'body' => 'これは最初の投稿の本文です。',
+            'user_id' => 4,
+            'title' => '初めて学ぶのにおすすめのプログラミング言語は何ですか？',
+            'body' => 'こんにちは！初めてでしたら、PythonかJavaScriptがよくおすすめされますね。Pythonは文法が比較的シンプルで読みやすく、JavaScriptはウェブブラウザで結果をすぐ確認できるので、動いている実感を得やすいです。何を作りたいかによっても変わってくるので、目標から逆算するのも良いですよ。',
+        ]);
+
+        // トピック2
+        Post::create([
+            'user_id' => 3,
+            'title' => 'Webアプリ開発に適した言語の選び方を教えてください。（例：Python, Ruby, PHP, JavaScriptなど）',
+            'body' => '', // こちらは回答がないため空文字としています。null にする場合は Post モデルの $fillable 設定やDB定義を確認してください。
         ]);
     }
 }
